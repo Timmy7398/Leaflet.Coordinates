1824de811d86e169b8bdf3ebdc8ae02b156d374d
friont <thomas.frion@etu.univ-grenoble-alpes.fr>
2020-07-15 09:44:35 +0200

    [WIP] Add UTM conversion

--- src/util/NumberFormatter.js
+++ src/util/NumberFormatter.js
@@ -1,4 +1,7 @@
 L.NumberFormatter = {
+
+	src: "+proj=longlat +datum=WGS84 +no_defs", // Projection WGS84
+
 	round: function(num, dec, sep) {
 		var res     = L.Util.formatNum(num, dec) + "",
 			numbers = res.split(".");
@@ -32,6 +35,29 @@ L.NumberFormatter = {
 		m = ('0' + (m % 60)).slice(-2);
 		
 		return ("" + dir + d + "&deg; " + m + "' " + s + "''");
+	},
+
+	zoneUTM: function(latLng){
+		const north = ['N','P','Q','R','S','T','U','V','W','X']; // Zones North Equator
+		const south = ['M','L','K','J','H','G','F','E','D','C']; // Zones South Equator
+		const zone  = Math.ceil((Math.round(latLng.lng) + 180) / 6);
+		
+		let lat  = Math.round(latLng.lat);
+		let i    = Math.abs((lat - (lat % 8)) / 8);
+
+		return { 
+			zone: zone, 
+			part: (lat > 0) ? north[i] : south[i],
+		}
+		 
+	},
+
+	toUTM: function(latLng) {
+		const zone  = this.zoneUTM(latLng);
+		const south = latLng.lat < 0 ? "+south" : "";
+		const dest  = '+proj=utm +zone='+zone.zone+' ' + south + ' +datum=WGS84 +units=m +no_defs';
+
+		return {...zone, ...{proj: proj4['default'](src,dest,[lng,lat])}};
 	},
 
 	createValidNumber: function(num, sep) {
